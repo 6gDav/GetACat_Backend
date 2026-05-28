184e7c12566915fcff39bb1f610f55d0e0028cda
6gDav <c6gdavprog0402@gmail.com>
2026-05-28 17:18:11 +0200

    feat: json conceted with api end pont and tested in Bruno

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import getCatPictures from './components/catImageFinder'
 import limiter from './components/rateLimiter'
+import catDatabase from './components/cats.json'
 
 type Varibles = {
   catImages: string[]
@@ -14,16 +15,28 @@ app.use('*', cors({
   allowMethods: ['GET'],
 }))
 
-app.use('*', async (c, next) => {
-  const images = await getCatPictures();
-  c.set('catImages', images);
-  await next();
-});
-
 
 app.get('/get-a-cat-image', limiter, async (c) => {
-  const images = c.get('catImages');
+  const images = await getCatPictures();
   return c.json(images)
+});
+
+app.get('/get-a-cat-info/:id', limiter, async (c) => {
+
+  const catId = c.req.param('id');
+
+  const catInfo = catDatabase.find((cat) => cat.id === catId);
+  if (!catInfo) {
+    return c.json(
+      {
+        success: false,
+        message: `Cat with ID '${catId}' not found.`
+      },
+      404
+    );
+  }
+
+  return c.json(catInfo);
 });
 
 export default {
